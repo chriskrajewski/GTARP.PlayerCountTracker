fdaa0e2f52612d56269bfdc7a3e139319c10926c
Chris Krajewski <ckrajewski@mwcomponents.com>
2025-05-05 22:44:55 -0400

    add statsig and gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,14 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+}): React.JSX.Element {
   return (
     <html lang="en">
+      <head>
+        <script src="https://cdn.jsdelivr.net/npm/@statsig/js-client@3/build/statsig-js-client+session-replay+web-analytics.min.js?apikey=client-Nu49JS6kPL97gZnvHVQZF64xQpf7aCGgRMdLm3wrEt5">
+        </script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-546BM3B67W"></script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <Suspense>
